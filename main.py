870cb96dc3eb56f71c78b7e73b62e0fd34253828
andy <ritmorica@gmail.com>
2024-05-23 16:47:17 +0300

    first commet 3

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ from datetime import datetime
 current_day_of_year = datetime.now().timetuple().tm_yday
 hour = datetime.now().hour
 minute = datetime.now().minute
-print(f"Сегодня {current_day_of_year}-й день в году и {minute}")
+print(f"Сегодня {current_day_of_year}-й день в году "
+      f"и время {hour}:{minute}")
 
 if __name__ == '__main__':
     asyncio.run(main_react())
 
-    if hour == 6 and 0 < minute <= 15:
-
+    if hour == 6 and 0 <= minute < 15:
         if current_day_of_year % 2 == 0:
             asyncio.run(check_pravda())
 
