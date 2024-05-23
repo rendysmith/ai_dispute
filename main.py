4ddedcba76d8f2a4417267a91de6ab6bcd0e68ab
andy <ritmorica@gmail.com>
2024-05-23 10:33:50 +0300

    first commet

---
+++ main.py
@@ -0,0 +1,12 @@
+import asyncio
+
+from ai_pref_reactions import main_react
+from ai_skillbox import check_pravda, check_ocompanii
+
+
+if __name__ == '__main__':
+    #asyncio.run(main_react())
+
+    #asyncio.run(check_pravda())
+    asyncio.run(check_ocompanii())
+
