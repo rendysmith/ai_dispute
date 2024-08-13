19f3d2fea90e961ebaab8d560ee0098c94ab91e0
andy <ritmorica@gmail.com>
2024-08-13 15:48:37 +0300

    fix: add new portal BA

--- portals/brandanalytics.py
+++ portals/brandanalytics.py
@@ -1,6 +1,9 @@
 import asyncio
-import aiohttp
+import json
 import os
+
+import aiohttp
+from cloudscraper import session
 from dotenv import load_dotenv
 
 from selenium.webdriver.common.by import By
@@ -15,6 +18,32 @@ load_dotenv(dotenv_path)
 username = os.environ.get("LOGIN_DA")
 password = os.environ.get("PASS_DA")
 
+async def get_cookies():
+    url = 'https://brandanalytics.ru/account/login_check'
+
+    
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url)
+
+
+async def get_json(driver):
+    base_url = "https://brandanalytics.ru/theme-data/12551940/"
+    params = "?tst=1723496399&tsf=1723237200&requested[]=feed&sort=time_create&order=desc&page=2&limit=25&filter[ft][not][]=30008&filter[ft][not][]=30009&filter[ft][not][]=15&filter[ft][not][]=30059&filter[ft][not][]=30025&filter[fmsgproc][any][]=1"
+    url = base_url + params
+
+    driver.get(url)
+    json_text = driver.find_element(By.TAG_NAME, "body").text
+
+    # Парсим текст как JSON
+    json_data = json.loads(json_text)
+
+    # Выводим полученные данные
+    print(type(json_data))
+    print(json_data)
+
+
+
 async def check_brandanalytics(url):
 
     driver = await get_selenium(url, headless=False)
@@ -28,6 +57,7 @@ async def check_brandanalytics(url):
     pass_input.send_keys(Keys.ENTER)
 
     driver.get(url)
+    await get_json(driver)
 
     len_span = 0
     while len_span == 0:
@@ -37,12 +67,13 @@ async def check_brandanalytics(url):
         await asyncio.sleep(3)
 
     for page in pages:
-        pages = page.text
+        pages = int(page.text)
 
     wait = WebDriverWait(driver, 10)
     #print(driver.page_source)
-
+    await get_json(driver)
     for i in range(pages):
+
         len_blocks = 0
         while len_blocks == 0:
             blocks = driver.find_elements(By.CSS_SELECTOR, 'div[class="feed_item feed_item__small"]')
@@ -51,7 +82,11 @@ async def check_brandanalytics(url):
             await asyncio.sleep(3)
 
         for block in blocks:
-            pass
+            date = block.find_element(By.CSS_SELECTOR, 'div[class="msg_date info_item"]').text
+            print(date)
+
+            feedback = block.find_element(By.CSS_SELECTOR, '')
+
 
 
         new_url = f"{url[:-1]}{i+2}"
@@ -76,6 +111,6 @@ async def main(url):
 
 if "__main__" in __name__:
     url = "https://brandanalytics.ru/report/12551940/summary?tsf=1723237200&tst=1723496399&ft%5Bnot%5D=30008&ft%5Bnot%5D=30009&ft%5Bnot%5D=15&ft%5Bnot%5D=30059&ft%5Bnot%5D=30025&fmsgproc%5Bany%5D=1&page=1"
-    url = "https://brandanalytics.ru/report/12551940/summary?tsf=1723237200&tst=1723496399&fmsgproc[any]=1&page=1"
+    #url = "https://brandanalytics.ru/report/12551940/summary?tsf=1723237200&tst=1723496399&fmsgproc[any]=1&page=1"
 
     asyncio.run(main(url))
