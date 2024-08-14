50045126c6a82c7877fe751ca61b34ce53a46779
andy <ritmorica@gmail.com>
2024-08-14 10:42:22 +0300

    fix: edit for continue

--- portals/brandanalytics.py
+++ portals/brandanalytics.py
@@ -9,17 +9,20 @@ from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 from pydantic.validators import datetime
 
+from utils.user_agent import get_soup
+
 """
 Данное упоминание нам не подходит:
 1) Упоминание находится в закрытом сообществе
 2) Упоминание находится на личной странице того или иного пользователя
+
 4) Тред мертв (то есть за обсуждаемую тему давно забыли и смысла отвечать на упоминание, которое было написано в этом обсуждени, смысла нет)
 5) Тред ушел (упоминание ушло далеко вверх и в группе давно обсуждается другая тема уже)
 7) Упоминание не о продукт (то есть данное упоминание тинькофф банк обходит стороной, либо же он упоминается там просто вскользь, так скажем)
 8) Обобщенное упоминание (автор говорит в целом о банках, а не конкретно о тинькофф. Может просто перечислять их)
-9) Упоминание размещено в аккаунте технического аккаунта (бота) (это могут быть какие-то посты в сообщетсвах, 
+9) Упоминание размещено в аккаунте технического аккаунта (бота) (это могут быть какие-то посты в сообществах, 
 которые, к примеру, каждый день закидывает бот. 
-Там нам так же смысла реагировать нет, поскольку мало вероятно что наше вовлечение как-то заметят)"""
+"""
 
 dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
 load_dotenv(dotenv_path)
@@ -46,20 +49,20 @@ async def get_cookies() -> dict:
                 raise Exception(f"Request failed with status code {response.status}")
 
 
-async def check_brandanalytics(url):
+async def check_brandanalytics():
 
     url_base = 'https://brandanalytics.ru/theme-data/12551940/'
 
-    tst = int(time.time() - 1 * 24 * 3600 )
+    tst = int(time.time())
     print(datetime.utcfromtimestamp(tst).strftime('%Y-%m-%d %H:%M:%S'))
 
-    tsf = int(time.time()  - 4 * 24 * 3600)
+    tsf = int(time.time() - 2 * 24 * 3600)
     print(datetime.utcfromtimestamp(tsf).strftime('%Y-%m-%d %H:%M:%S'))
 
     page = 1
     limit = 100
 
-    query = f'?tst={tst}&tsf={tsf}&requested%5B%5D=feed&sort=time_create&order=desc&page=1&limit=100&filter%5Bft%5D%5Bnot%5D%5B%5D=30008&filter%5Bft%5D%5Bnot%5D%5B%5D=30009&filter%5Bft%5D%5Bnot%5D%5B%5D=15&filter%5Bft%5D%5Bnot%5D%5B%5D=30059&filter%5Bft%5D%5Bnot%5D%5B%5D=30025&filter%5Bfmsgproc%5D%5Bany%5D%5B%5D=1'
+    query = f'?tst={tst}&tsf={tsf}&requested%5B%5D=feed&sort=time_create&order=desc&page={page}&limit={limit}&filter%5Bft%5D%5Bnot%5D%5B%5D=30008&filter%5Bft%5D%5Bnot%5D%5B%5D=30009&filter%5Bft%5D%5Bnot%5D%5B%5D=15&filter%5Bft%5D%5Bnot%5D%5B%5D=30059&filter%5Bft%5D%5Bnot%5D%5B%5D=30025&filter%5Bfmsgproc%5D%5Bany%5D%5B%5D=1'
     url = url_base + query
     print(url)
 
@@ -93,10 +96,21 @@ async def check_brandanalytics(url):
         text = soup.get_text()
 
         print(date_create)
-        #print(url_answer)
+        print(url_answer)
         print(text)
 
-        #input('WAIT...')
+        print('=======================================')
+        soup = await get_soup(url_answer)
+
+        if "Message in a private group or channel" in soup:
+            continue
+
+        print(soup)
+
+
+        input('WAIT...')
+
+
 
 
 
@@ -104,14 +118,11 @@ async def check_brandanalytics(url):
     input('OK!')
 
 
-async def main(url):
-    await check_brandanalytics(url)
+async def main():
+    await check_brandanalytics()
     #cookies = await get_cookies()
     #print(cookies)
 
 
 if "__main__" in __name__:
-    url = "https://brandanalytics.ru/report/12551940/summary?tsf=1723237200&tst=1723496399&ft%5Bnot%5D=30008&ft%5Bnot%5D=30009&ft%5Bnot%5D=15&ft%5Bnot%5D=30059&ft%5Bnot%5D=30025&fmsgproc%5Bany%5D=1&page=1"
-    #url = "https://brandanalytics.ru/report/12551940/summary?tsf=1723237200&tst=1723496399&fmsgproc[any]=1&page=1"
-
-    asyncio.run(main(url))
+     asyncio.run(main())
