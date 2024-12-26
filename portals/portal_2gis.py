fd4f67f9472e34e5c3a800b19faa2059da5f7e28
andy <ritmorica@gmail.com>
2024-12-26 11:36:54 +0300

    fix: new ai sidorin

--- portals/portal_2gis.py
+++ portals/portal_2gis.py
@@ -1,3 +1,6 @@
+import json
+from pprint import pprint
+
 import asyncio
 import random
 
@@ -5,7 +8,7 @@ from selenium.webdriver.common.by import By
 
 from datetime import datetime, timedelta, timezone
 
-from utils.central_module import get_local_ip
+from utils.central_module import get_local_ip, wait_for_portal
 from utils.compressor import compress_string
 from utils.gs_editor import pars_url, get_service, append_data_to_sheet_scope
 from utils.ai_module import generate_and_white
@@ -32,7 +35,7 @@ if '176.124.192' in local_ip:
 
 else:
     print(f'local_ip: {local_ip}')
-    headless = False
+    headless = True
     proxy_on = False
     only_text = False
 
@@ -68,8 +71,7 @@ async def send_top_url(service, ss_id, project, url):
     await append_data_to_sheet_scope(service, ss_id, 'unique_url', datas)
     return id_obj, top_url
 
-async def soup_pars(id_org, pattern, criteria):
-
+async def soup_pars(service, links, id_org, pattern, criteria, ss_id, project):
     url = f'https://public-api.reviews.2gis.com/2.0/branches/{id_org}/reviews?limit=50&is_advertiser=true&fields=meta.providers,meta.branch_rating,meta.branch_reviews_count,meta.total_count,reviews.hiding_reason,reviews.is_verified&without_my_first_review=false&rated=true&sort_by=friends&key=b0209295-ae15-48b2-acb2-58309b333c37&locale=ru_RU'
     print(url)
 
@@ -114,13 +116,120 @@ async def soup_pars(id_org, pattern, criteria):
                                  pattern=pattern,
                                  criteria=criteria)
 
-async def selen_pars(service, links, top_url, pattern, criteria, ss_id, project):
+async def pars_json_data(script_text):
+    script_text = script_text.replace('\\"', '')
+    script_text = script_text.replace('\\', '')
+    # Найти начало JSON
+    json_start = script_text.find('"review":')
+    #print(json_start)
+    if json_start == -1:
+        raise ValueError("Начало JSON не найдено")
 
+    # Баланс скобок
+    balance = 0
+    json_end = None
+    turn_off = False
+
+    # Ищем конец JSON-объекта
+    len_s = len(script_text)
+
+    for i in range(json_start, len_s - json_start):
+
+        char = script_text[i]
+        if char == '{':
+            balance += 1
+            turn_off = True
+
+        elif char == '}':
+            balance -= 1
+
+        # Если баланс = 0, это конец JSON
+        if balance == 0 and turn_off:
+            json_end = i
+            print(f'Break {i}')
+            break
+
+    #print(json_end)
+    if json_end is None:
+        raise ValueError("Конец JSON не найден")
+
+    # Извлекаем JSON-строку
+    json_str = script_text[json_start:json_end + 1]
+
+
+    #print("Извлечённый JSON:", json_str)
+    json_str = "{" + json_str + "}"  # Предварительный JSON словарь
+    #print(json_str)
+
+    data_dict = json.loads(json_str)
+    #pprint(data_dict)
+
+    return data_dict
+
+async def selen_pars(service, links, top_url, pattern, criteria, ss_id, project):
     top_url = top_url + '/tab/reviews'
     driver = await get_selenium_proxy(top_url, headless=headless, proxy=proxy_on)
+    await wait_for_portal() #Время ожидания
+    json_data_content = driver.find_elements(By.CSS_SELECTOR, 'script')
 
-    blocks = driver.find_elements(By.CSS_SELECTOR, 'div._1k5soqfl')
-    print('Len_B =', len(blocks))
+    data_dict = None
+    for script in json_data_content:
+        script_text = script.get_attribute('innerHTML')
+        if 'initialState =' in script_text:
+            data_dict = await pars_json_data(script_text)
+            pprint(data_dict)
+            break
+
+    if not data_dict:
+        return
+
+    if not isinstance(data_dict, dict):
+        return
+
+    blocks = data_dict['review']
+
+    for k, block in blocks.items():
+        date_content = block['data']['date_created']
+        date = datetime.strptime(date_content, "%Y-%m-%dT%H:%M:%S.%f%z")
+
+        if (current_date - date) > timedelta(days=days_ago):
+            print(f'--- Отзыв старше {days_ago} дней. = {date}')
+            continue
+
+        url_answer = block['data']['id']
+        if url_answer in links:
+            print('Такой комментарий уже есть в списке')
+            continue
+
+        official_answer = block['data']['official_answer']
+        if not official_answer:
+            print("Уже есть ответ компании.")
+            continue
+
+        formatted_date = date.strftime("%d.%m.%Y")
+        print(formatted_date)
+
+        feedback = block['data']['text']
+
+        author = block['data']['user']['name']
+        author = f"{author}\n{url}"
+
+        await generate_and_white(service=service,
+                                 url_answer=url_answer,
+                                 author=author,
+                                 formatted_date=formatted_date,
+                                 ss_id=ss_id,
+                                 project=project,
+                                 feedback=feedback,
+                                 pattern=pattern,
+                                 criteria=criteria)
+
+    try:
+        driver.quit()
+    except:
+        pass
+
+
 
 async def check_2gis(service, url, pattern, criteria, ss_id, project):
     links = await pars_url(service, ss_id, project)
@@ -142,7 +251,6 @@ async def check_2gis(service, url, pattern, criteria, ss_id, project):
 
 
 async def main_2gis(url):
-
     service = await get_service()
     #playwright, browser, page = await get_playwright(url)
     await check_2gis(service, url, 1, 1, "1zk9x6rdVVGKgsKK_7jRwD4yN9sd745mzQv4jRrKbI9w", "НовикомБанк")
@@ -249,3 +357,113 @@ if __name__ == '__main__':
 #
 #     await browser.close()
 #     await playwright.stop()
+#
+# async def selen_pars(service, links, top_url, pattern, criteria, ss_id, project):
+#     top_url = top_url + '/tab/reviews'
+#     driver = await get_selenium_proxy(top_url, headless=headless, proxy=proxy_on)
+#     await wait_for_portal() #Время ожидания
+#     json_data_content = driver.find_elements(By.CSS_SELECTOR, 'script')
+#
+#     data_dict = None
+#     for script in json_data_content:
+#         script_text = script.get_attribute('innerHTML')
+#         if 'initialState =' in script_text:
+#             data_dict = await pars_json_data(script_text)
+#             pprint(data_dict)
+#             break
+#
+#     if not data_dict:
+#         return
+#
+#     if not isinstance(data_dict, dict):
+#         return
+#
+#     pprint(data_dict)
+#     input('OK?')
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#     try:
+#         blocks = driver.find_elements(By.CSS_SELECTOR, 'div._1k5soqfl')
+#         len_b = len(blocks)
+#         print('Len_B =', len_b)
+#
+#     except:
+#         return
+#
+#     if len_b == 0:
+#         return
+#
+#
+#     input()
+#
+#
+#     for block in blocks:
+#         try:
+#             date_content = block.find_element(By.CSS_SELECTOR, 'div[class="_4mwq3d"]').text()
+#             date_split = date_content.split(', ')[0].split(' ')
+#             date_split = [dt.replace(',', '') for dt in date_split]
+#
+#         except Exception as Ex:
+#             print(f'Ex: {Ex}')
+#             date_content = block.find_element(By.CSS_SELECTOR, 'div[class="_139ll30"]').text()
+#             date_split = date_content.split(' ')
+#             date_split = [dt.replace(',', '') for dt in date_split]
+#
+#         day = int(date_split[0])
+#         month = months(date_split[1])
+#         year = int(date_split[2])
+#
+#         target_date = datetime(year, month, day)
+#         formatted_date = target_date.strftime("%d.%m.%Y")
+#
+#         if (current_date - target_date) > timedelta(days=days_ago):
+#             print(f'--- Отзыв старше {days_ago} дней = {date}.')
+#             continue
+#
+#         try:
+#             answer = block.find_element(By.CSS_SELECTOR, 'div[class="_sgs1pz"]').text()
+#             print('Уже есть ответ на комментарий')
+#             continue
+#
+#         except:
+#             pass
+#
+#         author_content = block.find_element(By.CSS_SELECTOR, 'span[class="_16s5yj36"]').text()
+#         author = author_content.strip()
+#
+#         feedback = block.find_element(By.CSS_SELECTOR, 'div[class="_49x36f"]').text()
+#
+#         url_answer = await compress_string(feedback)
+#
+#         if url_answer in links:
+#             print('Такой комментарий уже есть в списке')
+#             continue
+#
+#         author = f"{author}\n{url}"
+#
+#         await generate_and_white(service=service,
+#                                  url_answer=url_answer,
+#                                  author=author,
+#                                  formatted_date=formatted_date,
+#                                  ss_id=ss_id,
+#                                  project=project,
+#                                  feedback=feedback,
+#                                  pattern=pattern,
+#                                  criteria=criteria)
+#
+#     driver.quit()
