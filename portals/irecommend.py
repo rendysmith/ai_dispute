5f3a068c59af478c8687f6b5518573663b7257b3
andrewsmith <ritmorica@gmail.com>
2025-05-14 18:19:31 +0300

    fix: Sidorin

--- portals/irecommend.py
+++ portals/irecommend.py
@@ -11,6 +11,8 @@ from dotenv import load_dotenv
 from selenium.common import NoSuchWindowException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 from utils.ai_module import generate_and_white
@@ -44,8 +46,24 @@ async def click_checkbox(driver):
     n = 0
     while n < 20:
         try:
+            iframe_locator = driver.find_element(By.XPATH, "//iframe[contains(@title, 'challenge') or contains(@name, 'cf-chl-widget')]")
+            WebDriverWait(driver, 20).until(
+                EC.frame_to_be_available_and_switch_to_it(iframe_locator)
+            )
+            print("Switched to Cloudflare iframe.")
+
             click_box = driver.find_element(By.CSS_SELECTOR, 'input[type="checkbox"]')
-            click_box.click()
+            checkbox_element = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable(click_box)
+            )
+            print("Checkbox element found.")
+            checkbox_element.click()
+
+            print("Checkbox clicked successfully.")
+            driver.switch_to.default_content()
+            print("Switched back to default content.")
+
+            #click_box.click()
             return driver
 
         except:
