44111d81c8f587bf79288868299f2ff134096b15
andrewsmith <ritmorica@gmail.com>
2025-05-19 18:44:00 +0300

    fix: Sidorin

--- tst_cpu.py
+++ tst_cpu.py
@@ -1,26 +1,50 @@
-import asyncio
-import time
+import os
+from dotenv import load_dotenv
 
-import aiohttp
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
-async def make_request(semaphore, url):
-    async with semaphore:
-        async with aiohttp.ClientSession() as session:  # Используйте aiohttp для асинхронных запросов
-            async with session.get(url) as response:
-                await response.text()  # Или response.read() для бинарных данных
-                return response.status
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+load_dotenv(dotenv_path)
 
-async def tst_concurrency(url, n):
-    semaphore = asyncio.Semaphore(n)
-    tasks = [make_request(semaphore, url) for _ in range(100)] # 100 тестовых запросов
-    start_time = time.time()
-    results = await asyncio.gather(*tasks)
-    end_time = time.time()
-    print(f"Concurrency: {n}, Time: {end_time - start_time}, Errors: {sum(1 for status in results if status >= 400)}")
+login_proxy = os.environ.get("LOGIN_PROXY")
+pass_proxy = os.environ.get("PASS_PROXY")
 
-async def main():
-    url = "https://example.com"  # Замените на ваш URL
-    for n in [1, 5, 10, 20, 50]:
-        await tst_concurrency(url, n)
+async def func_1():
 
-asyncio.run(main())
+    proxy_host, proxy_port = await get_one_proxy()
+    print(proxy_host)
+
+    # Define custom options for the Selenium driver
+    options = Options()
+    proxy_server_url = f"https://{login_proxy}:{pass_proxy}@{proxy_host}:{proxy_port}"
+    options.add_argument(f'--proxy-server={proxy_server_url}')
+
+    # Create the ChromeDriver instance with custom options
+    driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()),
+        options=options
+    )
+
+    input(1)
+
+
+async def get_selenium_win(url=None, headless=True, proxy=True):
+    proxy = "username:password@proxy_address:proxy_port"
+    proxy_host, proxy_port = await get_one_proxy()
+    print(proxy_host)
+    proxy = f'{login_proxy}:{pass_proxy}@{proxy_host}:{proxy_port}'
+
+    chrome_options = Options()
+    chrome_options.add_argument(f'--proxy-server=http://{proxy}')
+
+    driver = webdriver.Chrome(options=chrome_options)
+    input(2)
+
+    if url:
+        driver.get(url)
+
+    return driver
