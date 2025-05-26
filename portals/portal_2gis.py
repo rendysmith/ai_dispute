7d0168b5bd75556642336d446ce67c04efee4070
andrewsmith <ritmorica@gmail.com>
2025-05-26 17:14:22 +0300

    Fix yandex

--- portals/portal_2gis.py
+++ portals/portal_2gis.py
@@ -72,7 +72,7 @@ async def send_top_url(service, ss_id, project, url):
     return id_obj, top_url
 
 async def soup_pars(service, links, id_org, pattern, criteria, ss_id, project):
-    url = f'https://public-api.reviews.2gis.com/2.0/branches/{id_org}/reviews?limit=50&is_advertiser=true&fields=meta.providers,meta.branch_rating,meta.branch_reviews_count,meta.total_count,reviews.hiding_reason,reviews.is_verified&without_my_first_review=false&rated=true&sort_by=friends&key=b0209295-ae15-48b2-acb2-58309b333c37&locale=ru_RU'
+    url = f'https://public-api.reviews.2gis.com/2.0/branches/{id_org}/reviews?limit=50&is_advertiser=true&fields=meta.providers,meta.branch_rating,meta.branch_reviews_count,meta.total_count,reviews.hiding_reason,reviews.is_verified&without_my_first_review=false&rated=true&sort_by=date_created&key=b0209295-ae15-48b2-acb2-58309b333c37&locale=ru_RU'
     print(url)
 
     headless, proxy_on, only_text = await get_hpo()
@@ -87,7 +87,7 @@ async def soup_pars(service, links, id_org, pattern, criteria, ss_id, project):
 
         if (current_date - date) > timedelta(days=days_ago):
             print(f'--- Отзыв старше {days_ago} дней. = {date}')
-            continue
+            return
 
         url_answer = block['id']
         if url_answer in links:
@@ -253,214 +253,7 @@ if __name__ == '__main__':
 
     url = 'https://2gis.ru/irkutsk/geo/1548773796872222/104.211407%2C52.355866?m=104.209969%2C52.352144%2F16'
     url = 'https://2gis.ru/firm/70000001006477930'
-    url = 'https://2gis.ru/firm/70000001059923460'
 
     asyncio.run(main_2gis(url))
     print('OK!')
 
-
-
-
-# async def check_2gis_old(service, url, pattern, criteria, ss_id, project, playwright, browser, page):
-#     #playwright, browser, page = await get_playwright(url)
-#
-#     if not page:
-#         # await browser.close()
-#         # await playwright.stop()
-#         return "Сайт не вернул данные"
-#
-#     if 'go' in url:
-#         final_url = page.url
-#         print("final_url:", final_url)
-#         await send_top_url(service, ss_id, project, final_url)
-#
-#     else:
-#         await send_top_url(service, ss_id, project, url)
-#
-#     links = await pars_url(service, ss_id, project)
-#
-#     blocks = await page.query_selector_all('div[class="_11gvyqv"]')
-#     print('Len =', len(blocks))
-#
-#     if len(blocks) == 0:
-#         await browser.close()
-#         await playwright.stop()
-#         return
-#
-#     for block in blocks:
-#         try:
-#             date_content = await block.query_selector('div[class="_4mwq3d"]')
-#             date = await date_content.inner_text()
-#             date_split = date.split(', ')[0].split(' ')
-#             date_split = [dt.replace(',', '') for dt in date_split]
-#             #print(date_split)
-#
-#         except AttributeError as AE:
-#             print(f'AE: {AE}')
-#             date_content = await block.query_selector('div[class="_139ll30"]')
-#             date = await date_content.inner_text()
-#             date_split = date.split(' ')
-#             date_split = [dt.replace(',', '') for dt in date_split]
-#             #print(date_split)
-#             print(f'AE: OK!')
-#
-#         day = int(date_split[0])
-#         month = months(date_split[1])
-#         year = int(date_split[2])
-#
-#         target_date = datetime(year, month, day)
-#         formatted_date = target_date.strftime("%d.%m.%Y")
-#         #print(formatted_date)
-#
-#         if (current_date - target_date) > timedelta(days=days_ago):
-#             print(f'--- Отзыв старше {days_ago} дней = {date}.')
-#             continue
-#
-#         try:
-#             answer_content = block.query_selector('div[class="_sgs1pz"]')
-#             answer = answer_content.inner_text()
-#             print('Уже есть ответ на комментарий')
-#             continue
-#
-#         except:
-#             pass
-#
-#         author_content = await block.query_selector('span[class="_16s5yj36"]')
-#         author = await author_content.inner_text()
-#         author = author.strip()
-#         #print('\n', author)
-#
-#         feedback_content = await block.query_selector('div[class="_49x36f"]')
-#         feedback = await feedback_content.inner_text()
-#         #print(feedback)
-#
-#         url_answer = await compress_string(feedback)
-#
-#         if url_answer in links:
-#             print('Такой комментарий уже есть в списке')
-#             continue
-#
-#         author = f"{author}\n{url}"
-#
-#         await generate_and_white(service=service,
-#                                  url_answer=url_answer,
-#                                  author=author,
-#                                  formatted_date=formatted_date,
-#                                  ss_id=ss_id,
-#                                  project=project,
-#                                  feedback=feedback,
-#                                  pattern=pattern,
-#                                  criteria=criteria)
-#
-#     await browser.close()
-#     await playwright.stop()
-#
-# async def selen_pars(service, links, top_url, pattern, criteria, ss_id, project):
-#     top_url = top_url + '/tab/reviews'
-#     driver = await get_selenium_proxy(top_url, headless=headless, proxy=proxy_on)
-#     await wait_for_portal() #Время ожидания
-#     json_data_content = driver.find_elements(By.CSS_SELECTOR, 'script')
-#
-#     data_dict = None
-#     for script in json_data_content:
-#         script_text = script.get_attribute('innerHTML')
-#         if 'initialState =' in script_text:
-#             data_dict = await pars_json_data(script_text)
-#             pprint(data_dict)
-#             break
-#
-#     if not data_dict:
-#         return
-#
-#     if not isinstance(data_dict, dict):
-#         return
-#
-#     pprint(data_dict)
-#     input('OK?')
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#     try:
-#         blocks = driver.find_elements(By.CSS_SELECTOR, 'div._1k5soqfl')
-#         len_b = len(blocks)
-#         print('Len_B =', len_b)
-#
-#     except:
-#         return
-#
-#     if len_b == 0:
-#         return
-#
-#
-#     input()
-#
-#
-#     for block in blocks:
-#         try:
-#             date_content = block.find_element(By.CSS_SELECTOR, 'div[class="_4mwq3d"]').text()
-#             date_split = date_content.split(', ')[0].split(' ')
-#             date_split = [dt.replace(',', '') for dt in date_split]
-#
-#         except Exception as Ex:
-#             print(f'Ex: {Ex}')
-#             date_content = block.find_element(By.CSS_SELECTOR, 'div[class="_139ll30"]').text()
-#             date_split = date_content.split(' ')
-#             date_split = [dt.replace(',', '') for dt in date_split]
-#
-#         day = int(date_split[0])
-#         month = months(date_split[1])
-#         year = int(date_split[2])
-#
-#         target_date = datetime(year, month, day)
-#         formatted_date = target_date.strftime("%d.%m.%Y")
-#
-#         if (current_date - target_date) > timedelta(days=days_ago):
-#             print(f'--- Отзыв старше {days_ago} дней = {date}.')
-#             continue
-#
-#         try:
-#             answer = block.find_element(By.CSS_SELECTOR, 'div[class="_sgs1pz"]').text()
-#             print('Уже есть ответ на комментарий')
-#             continue
-#
-#         except:
-#             pass
-#
-#         author_content = block.find_element(By.CSS_SELECTOR, 'span[class="_16s5yj36"]').text()
-#         author = author_content.strip()
-#
-#         feedback = block.find_element(By.CSS_SELECTOR, 'div[class="_49x36f"]').text()
-#
-#         url_answer = await compress_string(feedback)
-#
-#         if url_answer in links:
-#             print('Такой комментарий уже есть в списке')
-#             continue
-#
-#         author = f"{author}\n{url}"
-#
-#         await generate_and_white(service=service,
-#                                  url_answer=url_answer,
-#                                  author=author,
-#                                  formatted_date=formatted_date,
-#                                  ss_id=ss_id,
-#                                  project=project,
-#                                  feedback=feedback,
-#                                  pattern=pattern,
-#                                  criteria=criteria)
-#
-#     driver.quit()
