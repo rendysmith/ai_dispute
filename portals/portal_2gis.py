a0914598e311e09ec0eca9ee4973f43314094b65
andrewsmith <ritmorica@gmail.com>
2025-10-15 12:19:59 +0300

    fix: 2gis 2

--- portals/portal_2gis.py
+++ portals/portal_2gis.py
@@ -27,6 +27,7 @@ days_ago = int(os.environ.get("DAYS_AGO"))
 max_sec = int(os.environ.get("MAX_SEC"))
 
 headless, proxy_on, only_text = asyncio.run(get_hpo())
+proxy_on = False
 
 async def text_to_json(script_text, start_word):
     json_start = script_text.find(start_word)
